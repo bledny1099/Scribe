9edf61631451d79908f038e791926ed70fed1640
bledny1099 <laperouse101@proton.me>
2026-08-21 14:58:10 +0300

    feat: add Homebrew cask support, green glow on story section, updated subtitle and story text

---
+++ Casks/scribe.rb
@@ -0,0 +1,19 @@
+cask "scribe" do
+  version "2.4.2"
+  sha256 "e1d904c5c425fbc38fe3b82fc5e639853f5fc7fb8c2d0d4b9b420e1745f60709"
+
+  url "https://github.com/bledny1099/Scribe/releases/download/v#{version}/Scribe.dmg"
+  name "Scribe"
+  desc "Fast, private, customizable, open-source dictation for macOS"
+  homepage "https://github.com/bledny1099/Scribe"
+
+  depends_on macos: ">= :sonoma"
+
+  app "Scribe.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.aleksei.scribe",
+    "~/Library/Caches/com.aleksei.scribe",
+    "~/Library/Preferences/com.aleksei.scribe.plist",
+  ]
+end
